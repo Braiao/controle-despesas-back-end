c707d02a2697a2b12c959496371fb1faf206102f
Brayan <brayan_pessoa@outlook.com>
2024-11-26 16:15:53 -0300

    feat: env-configuration-files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      database: process.env.DATABASE_NAME,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      host: process.env.DATABASE_HOST,
-      port: 5432,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+     EnvConfigModule,
   ],
   controllers: [AppController],
   providers: [AppService],
