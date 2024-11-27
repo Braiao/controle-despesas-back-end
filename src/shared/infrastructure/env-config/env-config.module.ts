6d2930af19ba0f142e46abc1b8aa728ca320d6e1
Brayan <brayan_pessoa@outlook.com>
2024-11-27 07:11:38 -0300

    feat: refactioring env-config.module

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,23 @@
-import { Module } from '@nestjs/common';
-import { EnvConfigService } from './env-config.service';
+import { DynamicModule, Module } from '@nestjs/common'
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config'
+import { join } from 'node:path'
 
-@Module({
-  providers: [EnvConfigService]
-})
-export class EnvConfigModule {}
+@Module({})
+export class EnvConfigModule {
+    static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
+        return {
+            module: EnvConfigModule, // Define explicitamente o módulo
+            imports: [
+                ConfigModule.forRoot({
+                    ...options, // Permite opções personalizáveis
+                    envFilePath: [
+                        join(
+                            __dirname,
+                            `../../../../.env.${process.env.NODE_ENV}`,
+                        ), // Define o caminho do arquivo .env
+                    ],
+                }),
+            ],
+        }
+    }
+}
